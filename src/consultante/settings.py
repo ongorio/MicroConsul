52fb2fa44fdd75909d5c614c362481f4f3fc312c
ongorio <ongorio5@gmail.com>
2025-08-03 19:20:13 -0600

    feat: Added static root

--- src/consultante/settings.py
+++ src/consultante/settings.py
@@ -161,9 +161,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    BASE_DIR / 'static_dev',
 ]
 
 # Default primary key field type
